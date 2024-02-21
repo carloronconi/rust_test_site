cfa2d1f633b6ee643cab8bf2f7cead8f319b2b91
carloronconi <carlo.ronconi@mail.polimi.it>
2024-02-21 10:09:37 +0100

    update command creation to parse word-by-word

--- src/bin/lib_collections/mod.rs
+++ src/bin/lib_collections/mod.rs
@@ -1,16 +1,13 @@
 use std::fmt::Error;
-use regex::Regex;
-
-const COMMAND_FORMAT: &str = "^(add membership .* to .*|remove membership .* from .*|detail membership .* from .*)$";
 
 pub trait RunCommand {
     fn submit(command: &str) -> Result<String, Error>;
 }
 
 pub enum Entity {
-    Group,
-    User,
-    Relation,
+    Group(String),
+    User(String),
+    Relation(String, String),
 }
 
 pub enum Command {
@@ -21,18 +18,23 @@ pub enum Command {
 
 impl Command {
     fn new(text: &str) -> Result<Self, Error> {
-        let reg = Regex::new(COMMAND_FORMAT).unwrap();
-        if !reg.is_match(text) { return Err(Error) }
 
         let mut words = text.split_whitespace();
 
-        let command = words.next().unwrap();
-        let entity = words.next().unwrap();
+        let command = words.next().ok_or(Error)?;
+        let entity = words.next().ok_or(Error)?;
+        let name1: String = words.next().ok_or(Error)?.parse().unwrap();
+        let connector = words.next();
+        let name2 = words.next();
+
 
         let entity = match entity {
-            "group" => Entity::Group,
-            "user" => Entity::User,
-            "relation" => Entity::Relation,
+            "group" => Entity::Group(name1),
+            "user" => Entity::User(name1),
+            "relation" => {
+                if !connector.ok_or(Error)?.eq("into") { return Err(Error); }
+                let name2 = name2.ok_or(Error)?.parse().unwrap();
+                Entity::Relation(name1, name2) },
             _ => return Err(Error),
         };
 
--- src/bin/lib_collections_main.rs
+++ src/bin/lib_collections_main.rs
@@ -7,13 +7,13 @@ fn main() {
     let mut input = String::new();
 
     loop {
-        // ((add|remove|detail) (group|user) [name])|(add/remove/detail membership [name-of-user] to/from [name-of-group])
+        // ((add|remove|detail) (group|user) [name])|(add/remove/detail membership [name-of-user] into [name-of-group])
 
         println!("Write a prompt to interact with the user datastore:\n
-            change groups `add/remove/detail group [name-of-group]'\n
-            change users `add/remove/detail user [name-of-user]'\n
-            change membership between users and groups 'add/remove/detail membership [name-of-user]\
-             to/from/from [name of group]'");
+            \t- change groups `add/remove/detail group [name-of-group]'\n
+            \t- change users `add/remove/detail user [name-of-user]'\n
+            \t- change membership between users and groups 'add/remove/detail membership [name-of-user]\
+            \t  into/into/into [name of group]'");
 
         io::stdin().read_line(&mut input).unwrap();
 
